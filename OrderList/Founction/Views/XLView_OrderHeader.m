7ff97bf6fd188da0c375357c02e2205e1549dcb3
trigkit4 <345823102@qq.com>
2017-12-18 14:32:08 +0800

    pch

--- OrderList/Founction/Views/XLView_OrderHeader.m
+++ OrderList/Founction/Views/XLView_OrderHeader.m
@@ -10,12 +10,19 @@
 
 @implementation XLView_OrderHeader
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if(self)
+    {
+        self.frame = frame;
+        [self createControls];
+    }
+    return self;
 }
-*/
+
+- (void)createControls {
+    
+}
 
 @end
