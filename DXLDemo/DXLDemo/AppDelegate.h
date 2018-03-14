86ec9a378a721cf82e9c0a539af187dc6c005eba
邓西亮 <972424506@qq.com>
2018-03-14 15:09:25 +0800

    日常Demo

---
+++ DXLDemo/DXLDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DXLDemo
+//
+//  Created by ddSoul on 2018/3/14.
+//  Copyright © 2018年 dxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
