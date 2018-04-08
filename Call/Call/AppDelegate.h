2999a3068c13ea976655baf07f68fdda9dba70f1
ddSoul <972424506@qq.com>
2018-04-08 14:19:54 +0800

    CallKit

---
+++ Call/Call/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Call
+//
+//  Created by ddSoul on 2018/2/27.
+//  Copyright © 2018年 dxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Call/com.dxl.callextersion.CallDirectoryExtension/CallDirectoryHandler.h
@@ -0,0 +1,15 @@
+//
+//  CallDirectoryHandler.h
+//  com.dxl.callextersion.CallDirectoryExtension
+//
+//  Created by ddSoul on 2018/2/27.
+//  Copyright © 2018年 dxl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CallKit/CallKit.h>
+
+@interface CallDirectoryHandler : CXCallDirectoryProvider
+
+@end
+
